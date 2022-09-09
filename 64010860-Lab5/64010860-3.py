50bf2a9e59935913281b0b1b0bbdbb5cf314aa47
16sakuraa <64010860@kmitl.ac.th>
2022-09-09 16:03:55 +0700

    64010860-3 finished

--- 64010860-Lab5/64010860-3.py
+++ 64010860-Lab5/64010860-3.py
@@ -2,50 +2,47 @@ class node:
     def __init__(self,data,next = None):
         self.data = data
         self.next = next
-    # def __str__(self):
-    #     if self.head is None:
-    #         print('This linked list is empty')
-    #         return
-        
-    #     itr = self.head
-    #     liststr = ''
 
-    #     while itr:
-    #         #print(itr)
-    #         if itr.next is None:
-    #             liststr += str(itr.data)
-    #         else:
-    #             liststr += str(itr.data) + ' <- '
-    #         itr = itr.next
-            
-    #     print(liststr)
-
-    #def traverse(self):
-
-def createList(L=[]):
-    newnode = node(L[0])
+def createList(List=[]):
+    L = [e for e in List.split()]
+    newnode = node(int(L[0]))
     head = newnode
-
     for i in range(0,len(L)-1):
-        tempnode = node(L[i+1])
+        tempnode = node(int(L[i+1]))
         newnode.next = tempnode
-        #print(i)
         newnode = tempnode
-
-
     return head
 
 def printList(H):
     itr = H
     y=''
     while itr:
-        y += str(itr.data)
+        y += str(itr.data)+' '
         itr = itr.next
-    
     print(y)
 
-# def mergeOrderesList(p,q):
-#     ### Code Here ###
+def mergeOrderesList(p,q):
+    dummy = node(0)
+    tail = dummy
+
+    while True:
+        if p is None:
+            tail.next = q
+            break
+        if q is None:
+            tail.next = p
+            break
+
+        if p.data <= q.data:
+            tail.next = p
+            p = p.next
+        else:
+            tail.next = q
+            q = q.next
+
+        tail = tail.next
+    
+    return dummy.next
 
 #################### FIX comand ####################   
 # input only a number save in L1,L2
@@ -58,6 +55,6 @@ print('LL1 : ',end='')
 printList(LL1)
 print('LL2 : ',end='')
 printList(LL2)
-# m = mergeOrderesList(LL1,LL2)
-# print('Merge Result : ',end='')
-# printList(m)
+m = mergeOrderesList(LL1,LL2)
+print('Merge Result : ',end='')
+printList(m)
