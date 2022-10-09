d9286b2c6542a41c0f7c1fc4fa376e05afe3b0f3
16sakuraa <64010860@kmitl.ac.th>
2022-10-09 21:09:07 +0700

    added lab7-2

--- 64010860-Lab7/64010860-2.py
+++ 64010860-Lab7/64010860-2.py
@@ -29,7 +29,7 @@ class BST:
                 currentNode.right = Node(data)
             else:
                 self._insert(data,currentNode.right)
-           
+
     
     def printTree(self, node, level = 0):
         if node != None:
@@ -37,8 +37,44 @@ class BST:
             print('     ' * level, node)
             self.printTree(node.left, level + 1)
 
+    def findBelow(self,node,num):
+        global list
+        if node:
+            if int(node.data) < int(num):
+                list.append(node.data)
+                self.findBelow(node.right,num)
+                self.findBelow(node.left,num)
+            else:
+                self.findBelow(node.right,num)
+                self.findBelow(node.left,num)
+               
+        return list
+
+def listSorting(l):
+        toStr = ''
+        for i in range(len(l)):
+            for j in range(i + 1, len(l)):
+
+                if l[i] > l[j]:
+                    l[i], l[j] = l[j], l[i]
+        
+        if len(l)==0:
+            print('Not have')
+        else:
+            for i in l:
+                toStr += str(i)+' '
+        print(toStr)
+
+
+list = []
 T = BST()
-inp = [int(i) for i in input('Enter Input : ').split()]
-for i in inp:
+inp,num = [i for i in input('Enter Input : ').split('|')]
+inpdata = [int(i) for i in inp.split()]
+for i in inpdata:
     root = T.insert(i)
 T.printTree(root)
+print('--------------------------------------------------')
+print('Below '+num+' : ',end='')
+res = T.findBelow(root,num)
+listSorting(res)
+
